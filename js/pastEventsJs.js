df2a0fa206cb9d7c9a10dd34c04fb8a286daa556
Rocio Morales <roncio.93@gmail.com>
2022-11-21 21:57:32 -0300

    :sparkles: Feat: add functionality to search input on home page, upcoming events page and past events page

--- js/pastEventsJs.js
+++ js/pastEventsJs.js
@@ -1,1 +1,19 @@
+paintDOMCategories(eventCategories)
 paintDOM(pastEvents)
+
+const inputEvent = document.getElementById("input-event")
+
+inputEvent.addEventListener("change", (event)=>{
+    console.log("event", event.target.value)
+    const search = event.target.value
+    let coincidences = pastEvents.filter(evento => (evento.name.toLowerCase().includes(search.toLowerCase()) || evento.description.toLowerCase().includes(search.toLowerCase())))
+    paintDOM(coincidences)
+})
+
+const searchButton = document.getElementById("search-button")
+
+searchButton.addEventListener("onclick", ()=>{
+    const inputValue = document.getElementById("input-event").value
+    let coincidences = pastEvents.filter(evento => (evento.name.toLowerCase().includes(inputValue.toLowerCase()) || evento.description.toLowerCase().includes(inputValue.toLowerCase())))
+    paintDOM(coincidences)
+})
--- js/upcomingEventsJs.js
+++ js/upcomingEventsJs.js
@@ -1,1 +1,19 @@
+paintDOMCategories(eventCategories)
 paintDOM(futureEvents)
+
+const inputEvent = document.getElementById("input-event")
+
+inputEvent.addEventListener("change", (event)=>{
+    console.log("event", event.target.value)
+    const search = event.target.value
+    let coincidences = futureEvents.filter(evento => (evento.name.toLowerCase().includes(search.toLowerCase()) || evento.description.toLowerCase().includes(search.toLowerCase())))
+    paintDOM(coincidences)
+})
+
+const searchButton = document.getElementById("search-button")
+
+searchButton.addEventListener("onclick", ()=>{
+    const inputValue = document.getElementById("input-event").value
+    let coincidences = futureEvents.filter(evento => (evento.name.toLowerCase().includes(inputValue.toLowerCase()) || evento.description.toLowerCase().includes(inputValue.toLowerCase())))
+    paintDOM(coincidences)
+})
